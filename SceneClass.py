74d52d645b3f3e76c49e5782a561d28aa34099dd
Park bobin <asapdodin@tukorea.ac.kr>
2025-10-15 23:24:50 +0900

    class 내부 draw 선언

--- SceneClass.py
+++ SceneClass.py
@@ -1,25 +1,40 @@
 from pico2d import load_image
 
 class BaceScene: #모든 화면이 공통적으로 수행하는 기능을 담는 클래스
-    def __init__(self):
-        pass
+    def __init__(self,scene):
+        self.scene = scene
 
 class SceneManager:
-    def __init__(self):
+    def __init__(self, scene):
+        self.scene = scene  # 현재 장면이 무슨 장면인지 받음
+    def handle(self):
         pass
+    def update(self):
+        pass
+    def draw(self):
+        self.scene.draw()
 
 class StartScene:
     def __init__(self):
         self.image = load_image('화면 리소스/시작 화면/background.png')
+    def draw(self):
+        pass
+
 
 class CharacterSelect:
     def __init__(self):
+        pass
+    def draw(self):
         pass
 
 class CostumeSelect:
     def __init__(self):
         pass
+    def draw(self):
+        pass
 
 class FightScene:
     def __init__(self):
+        pass
+    def draw(self):
         pass
