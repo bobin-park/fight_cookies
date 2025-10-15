84bb26b6181f062e5fbc9d3542ac000c32a5fcc0
Park bobin <asapdodin@tukorea.ac.kr>
2025-10-15 23:12:31 +0900

    각 scene class 생성

--- class.py
+++ class.py
@@ -1,3 +1,15 @@
 class BaceScene: #모든 화면이 공통적으로 수행하는 기능을 담는 클래스
     def __init__(self):
         pass
+
+class StartScene:
+    pass
+
+class CharacterSelect:
+    pass
+
+class CostumeSelect:
+    pass
+
+class FightScene:
+    pass
