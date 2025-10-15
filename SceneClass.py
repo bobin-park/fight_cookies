445771083d12bcf89f7cd79f0ad9a82a7f9f14e4
Park bobin <asapdodin@tukorea.ac.kr>
2025-10-15 23:22:48 +0900

    scenemanger class생성

--- SceneClass.py
+++ SceneClass.py
@@ -4,9 +4,13 @@ class BaceScene: #모든 화면이 공통적으로 수행하는 기능을 담는
     def __init__(self):
         pass
 
+class SceneManager:
+    def __init__(self):
+        pass
+
 class StartScene:
     def __init__(self):
-        self.image = load_image('')
+        self.image = load_image('화면 리소스/시작 화면/background.png')
 
 class CharacterSelect:
     def __init__(self):
