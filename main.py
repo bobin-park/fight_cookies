aadd4f1924177f69b325032caf3dc2ab399ee1dd
Park bobin <asapdodin@tukorea.ac.kr>
2025-10-15 23:49:06 +0900

    open_canvas

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from SceneClass import *
+from pico2d import *
 
 def start_game():
+    open_canvas()
+
     global screen
     screen = StartScene()
 
 def update_screen():
     SceneManager.update(screen)
 
+
+start_game()
 while 1:
+    update_screen()
     pass
